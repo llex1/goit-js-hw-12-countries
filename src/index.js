f8747077ed4bc6c08954a2f72ce6a6737b98575b
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2020-11-03 06:15:03 +0200

    add pnotify

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import css from "./scss/main.scss";
 import hbs_countryList from "./templates/country-list.hbs";
 import hbs_countryInfo from "./templates/country-info.hbs";
 import {debounce} from 'lodash';
-// import * as pnotify from '@pnotify/core';
-// import '@pnotify/core/dist/BrightTheme.css';
-
+import {notice} from "../node_modules/@pnotify/core/dist/PNotify.js";
+import * as PNotifyDesktop from "../node_modules/@pnotify/desktop/dist/PNotifyDesktop.js";
+import "../node_modules/@pnotify/core/dist/PNotify.css";
+import '@pnotify/core/dist/BrightTheme.css';
 
 
 const dom = {
@@ -21,7 +22,9 @@ dom.dataIn.addEventListener(
     fetch(`https://restcountries.eu/rest/v2/name/${e.target.value}`)
       .then((data) => data.json())
       .then((json) => {
-        if (json.length === 1) {
+        if (json.length > 10) {
+          notice("Уточніть пошук. Велика кількість співпадінь...");
+        } else if (json.length === 1) {
           let [ el1 ] = json;
           dom.dataOut.innerHTML = hbs_countryInfo(el1);
         } else if(json.length >1 && json.length<=10) {
